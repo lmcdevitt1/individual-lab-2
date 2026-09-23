550d17b349f9aed01f93eb3d5b663f182251207a
Luke McDevitt <lmcdevitt1@student.cscc.edu>
2026-09-23 15:03:15 -0400

    added item controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -9,43 +9,43 @@ class ItemController extends Controller
 {
     public function index()
     {
-        $item = {
+        $item =
             new Item([
                 'name' => 'Hot Coffee',
                 'size' => 'Small',
                 'price' => 2.49
-            ]),
-        new Item(attributes: [
+            ]);
+        new Item([
             'name' => 'Hot Coffee',
             'size' => 'Medium',
             'price' => 2.99
-        ])
-            ];
+        ]);
         new Item([
             'name' => 'Hot Coffee',
             'size' => 'Large',
             'price' => 3.49
-        ]),
+        ]);
         new Item([
             'name' => 'Hot Coffee',
             'size' => 'Extra Large',
             'price' => 3.89
-        ]),
+        ]);
         new Item([
             'name' => 'Iced Coffee',
             'size' => 'Small',
             'price' => 2.79
-        ]),
+        ]);
         new Item([
             'name' => 'Iced Coffee',
             'size' => 'Medium',
             'price' => 3.29
-        ]),
+        ]);
         new Item([
             'name' => 'Iced Coffee',
             'size' => 'Large',
             'price' => 3.79
-        ])
-            ];
+        ]);
 
-return view('app', ['items' => $item]);
+    }
+}
+return view('app', ['Items' => $item]);
