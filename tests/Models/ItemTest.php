2f280648b74396a0d1278bbdd85f47f5d97a5538
Luke McDevitt <lmcdevitt1@student.cscc.edu>
2026-09-23 15:06:59 -0400

    added resources file

---
+++ tests/Models/ItemTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Models;
+
+use App\Models\Item;
+use PHPUnit\Framework\TestCase;
+
+class ItemTest extends TestCase
+{
+
+    public function testToString()
+    {
+
+    }
+}
