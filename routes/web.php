971e0ddb2f3bc1a9e1b8fd021283575f66a49e0f
Luke McDevitt <lmcdevitt1@student.cscc.edu>
2026-09-24 10:32:02 -0400

    changed

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::controller(ItemController::class)->group(function () {
+    Route::get('/', 'welcome')->name('home');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::controller(ItemController::class)->group(function () {
+        Route::get('/dashboard', 'dashboard')->name('dashboard');
+    });
 });
 
 Route::get('/items', [ItemController::class, 'index']);
